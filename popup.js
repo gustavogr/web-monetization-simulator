7ffb08ae06e57751ef0b20cdbbafe7d587994ed4
Amin Arria <arria.amin@gmail.com>
2020-05-17 16:50:34 -0300

    Added fields to popup form

--- popup.js
+++ popup.js
@@ -2,7 +2,10 @@ document.getElementById('money-form').addEventListener("submit", (e) => {
     e.preventDefault();
 
     data = {
-        "amount" : new Number(e.target.amount.value)
+        "currency" : e.target.currency.value,
+        "amount" : new Number(e.target.amount.value),
+        "interval" : new Number(e.target.interval.value),
+        "limit" : new Number(e.target.limit.value)
     };
 
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
