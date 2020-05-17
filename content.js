1a49666cc72df45dcbe93f416fa365b04ac243c6
Amin Arria <arria.amin@gmail.com>
2020-05-17 12:36:58 -0300

    Dispatch money event from injected script

--- content.js
+++ content.js
@@ -1,8 +1,24 @@
 const script = `
   document.monetization = document.createElement("div");
   document.monetization.state = "stopped";
+
+  window.addEventListener("message", function(event) {
+    // We only accept messages from ourselves
+    if (event.source != window)
+      return;
+
+    if (event.data.type === "monetization_event") {
+      console.log("Recieved monetization event: " + event.data.event);
+      event = new CustomEvent("monetizationstart", {detail: event.data.event});
+      document.monetization.dispatchEvent(event);
+    }
+  }, false);
 `;
 
 const element = document.createElement("script");
 element.innerHTML = script;
 document.documentElement.appendChild(element);
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  window.postMessage({ type: "monetization_event", event: {somedaa: "thatdata"}}, "*");
+});
--- test.js
+++ test.js
@@ -1,1 +1,5 @@
 console.log("Monetization in test script:", document.monetization);
+
+document.monetization.addEventListener("monetizationstart", (e) => {
+  console.log("recieved money event", e);
+});
