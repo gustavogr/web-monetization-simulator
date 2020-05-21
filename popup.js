2899b57cc8372b4a07960ff5cd0ad91aa95a5bc7
Amin Arria <arria.amin@gmail.com>
2020-05-20 22:33:00 -0300

    Added scale field and changed field types to number

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ document.getElementById('money-form').addEventListener("submit", (e) => {
 
     data = {
         "currency" : e.target.currency.value,
+        "scale" : new Number(e.target.scale.value),
         "amount" : new Number(e.target.amount.value),
         "interval" : new Number(e.target.interval.value),
         "limit" : new Number(e.target.limit.value)
