a1f3d0877158adc382c4ebe70dceef40ed22bbbc
Gustavo Gutierrez <gustavo@acidtango.com>
2020-06-02 22:58:26 +0200

    task: using 128 pix image as default icon

--- pack.sh
+++ pack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir web-monetization-simulator
 cp -R LICENSE README.md {background,content,popup}.js popup.html manifest.json icons web-monetization-simulator
-zip -r -X web-monetization-simulator.zip web-monetization-simulator
+zip -r -X chrome-extension.zip web-monetization-simulator
+zip -r -X firefox-extension.zip LICENSE README.md {background,content,popup}.js popup.html manifest.json icons
 rm -rf web-monetization-simulator
