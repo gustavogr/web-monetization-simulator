8e95b95c21298556491c2c20a9a01ca086f431b5
Gustavo Gutierrez <gustavo@acidtango.com>
2020-06-03 21:15:03 +0200

    task: updated extension permissions

--- pack.sh
+++ pack.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-mkdir web-monetization-simulator
-cp -R LICENSE README.md {background,content,popup}.js popup.html manifest.json icons web-monetization-simulator
-zip -r -X chrome-extension.zip web-monetization-simulator
-zip -r -X firefox-extension.zip LICENSE README.md {background,content,popup}.js popup.html manifest.json icons
-rm -rf web-monetization-simulator
+zip -r -X web-monetization-simulator.zip LICENSE README.md {background,content,popup}.js popup.html manifest.json icons
